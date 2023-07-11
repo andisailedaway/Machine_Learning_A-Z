efb7edc83f7044be42f09ab8cc1435d8e3ccab6a
nhaberer4mile <andisailedaway@gmail.com>
2023-07-11 15:11:47 -0600

    testing email link

--- multipleregression/multipleregression.py
+++ multipleregression/multipleregression.py
@@ -2,5 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv("linearregression/Salary_Data.csv")
+dataset = pd.read_csv("linearregression/50_Startups.csv")
 
