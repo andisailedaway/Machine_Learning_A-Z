1657bba6f693268b96cbc684852bd9f0c31e4a44
nhaberer4mile <andisailedaway@gmail.com>
2023-07-13 18:59:27 -0600

    multilinear regression template

--- multipleregression/multipleregression.py
+++ multipleregression/multipleregression.py
@@ -2,5 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv("linearregression/50_Startups.csv")
+#import dataset
+dataset = pd.read_csv("multipleregression/50_Startups.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
+#encode categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(sparse=False), [3])], remainder='passthrough')
+# 3 = column number you would like to apply the transformation to
+X = np.array(ct.fit_transform(X))
+print(X)
+
+#split data into training set and test set
+from sklearn.model_selection import train_test_split
+X_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+
+#reshape vector into vertical column - concat predicted and actual results side by side
+print(np.concatenate((y_pred.reshape(len(y_pred), 1),y_test.reshape(len(y_test), 1)),1))
